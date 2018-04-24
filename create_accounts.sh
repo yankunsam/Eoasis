9d0e70ec85d0a92176df4dfafc4ef3f20b431e4f
Sam <samyankun@gmail.com>
2018-04-24 18:34:23 +0800

    shell script for new account

--- create_accounts.sh
+++ create_accounts.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-filename="./publickey.conf"
+filename="./accounts.conf"
 while read -r line
 do
     name="$line"
     echo "Name read from file - $name"
     accountname=$(echo $line | awk -F ":" '{print $1}')
     publickey=$(echo $line | awk -F ":" '{print $2}')
-    docker exec eosio cleos create account eosio $accountname $publickey $publickey
+    docker exec eos0 cleos create account eosio $accountname $publickey $publickey
 done < "$filename"
