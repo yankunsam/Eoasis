21c74876bef9cb35bd2b13db66278a633aec024b
sam <samyankun@gmail.com>
2018-04-26 10:43:13 +0800

    correct temporary file name

--- set_prods.sh
+++ set_prods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker cp ./setprods.json eos0:/root/data/setprods.json
+docker cp ./test.json eos0:/root/data/setprods.json
 docker exec eos0 cleos push action eosio setprods /root/data/setprods.json -p eosio@active
