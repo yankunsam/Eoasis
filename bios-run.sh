0f509bc150ec1f99e2880f60b951bc0754e1fd9b
sam <samyankun@gmail.com>
2018-05-01 13:46:18 +0800

    stop the exist eos0

--- bios-run.sh
+++ bios-run.sh
@@ -19,4 +19,8 @@ else
   echo "[INFO] delete exist $configdir"
 fi
 docker pull samyankun/eosbios:0.1
+if [ $(docker inspect | grep eos0) ]; then
+  echo "[INFO] stop the exist eos0 container"
+  docker stop --force eos0
+fi
 docker run -it --rm --hostname eosio -v /opt/data/:/opt/data -v /opt/config:/opt/config --name eos0 -p 9876:9876  $image:$tag /bin/bash
