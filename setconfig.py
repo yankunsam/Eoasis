716f97ec4a6ad4c1f89bac30f816a152730d001e
sam <samyankun@gmail.com>
2018-05-15 19:20:33 +0800

    generate a config file for bp or voters
    with hostname now

---
+++ setconfig.py
@@ -0,0 +1,21 @@
+import configparser
+import os
+from cleos import Cleos
+import subprocess
+config = configparser.ConfigParser()
+config.read("config.ini")
+accountname = os.uname()[1]
+config['nodeos']['accountname'] = accountname
+cleosinstance = Cleos("eosio")
+keylist = cleosinstance.createKey()
+config['nodeos']['privatekey'] = keylist[0]
+config['nodeos']['publickey'] = keylist[1]
+config['regproducer']['account'] = accountname
+config['regproducer']['url'] = "%s%s%s" %("www.",accountname,".com")
+config['delegatebw']['account'] = accountname
+config['delegatebw']['from'] = accountname
+config['delegatebw']['receiver'] = accountname
+config['voteproducer']['voter'] = accountname
+config['voteproducer']['account'] = accountname
+with open('config.inibk','w') as configfile:
+    config.write(configfile)
