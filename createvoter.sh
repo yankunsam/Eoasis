b162754b430e9bb4ef0a850b8f250c080f4ae330
sam <samyankun@gmail.com>
2018-05-17 07:21:37 +0800

    create voters

---
+++ createvoter.sh
@@ -0,0 +1,10 @@
+for i in `seq 1 50`
+do
+    bp=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 4 | head -n 1)
+    command=$(cleos create key)
+    privatekey=$(echo $command | awk '{print $3}')
+    echo "Privatekey:" $privatekey
+    cleos wallet import $privatekey
+    publickkey=$(echo $command | awk '{print $6}')
+    echo "$bp:$publickkey" >> voters.conf
+done
