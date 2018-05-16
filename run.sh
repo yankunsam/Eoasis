f514afaefd02fcd62f2b3ecc73ef280bbee69091
sam <samyankun@gmail.com>
2018-05-17 07:16:03 +0800

    remove eosio.token create

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 python main.py  startbios
 sleep 3
-python main.py createwallet >> walletpassword
+python main.py createwallet > walletpassword
 python main.py importbiosprivatekey
 sleep 5
 python main.py setbioscontract
-cleos wallet import 5Jpg774MBGe8dBv8Hc417nNetrDbQqeojx1eAT8CgU1VeiqRm6B
-cleos create account eosio eosio.token EOS84QUdWxpq8tprvwSXWwWzhvy9nZTjBeJeEhSsJyZcJEhyhXhce EOS84QUdWxpq8tprvwSXWwWzhvy9nZTjBeJeEhSsJyZcJEhyhXhce 
 python main.py createbpaccount
 python main.py settokencontract
 python main.py createtoken
 python main.py issuetoken
+python main.py currencytransfer
 #sleep 15
 #python main.py setsystemcontract
