057bad211dd41d2f893b86ac8029b1bcb828f7c2
sam <samyankun@gmail.com>
2018-05-18 00:16:38 +0800

    add a host

--- setp2paddress.py
+++ setp2paddress.py
@@ -10,12 +10,14 @@ config.read(configfile)
 biosnodeaddress="103.235.232.27:9876"
 bpaddress = []
 
-for i in range(9700,9700+26):
+for i in range(9700,9700+18):
     bpaddresstmp1="%s:%s" % ("103.235.232.21",i)
     bpaddresstmp2="%s:%s" % ("103.235.232.43",i)
+    bpaddresstmp3="%s:%s" % ("103.235.232.27",i)
     #bpaddresstmp="%s:%s" % ("192.168.0.127",i)
     bpaddress.append(bpaddresstmp1)
     bpaddress.append(bpaddresstmp2)
+    bpaddress.append(bpaddresstmp3)
 
 bpaddress = ','.join(bpaddress)
 print(bpaddress)
