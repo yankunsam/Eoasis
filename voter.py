4824d16df004891a7531d875bed6bd3a30b3c17b
sam <samyankun@gmail.com>
2018-05-17 17:36:03 +0800

    voter process

---
+++ voter.py
@@ -0,0 +1,28 @@
+import configparser
+import os
+import subprocess
+config = configparser.ConfigParser()
+dirname = os.path.dirname(__file__)
+configfile = os.path.join(dirname, './config.ini')
+config.read(configfile)
+bpcanidatelist = []
+for conffile in ["accounts.conf"]:
+    with open(conffile,'r') as f:
+        for line in f:
+            accountname = line.split(':')[0]
+            bpcanidatelist.append(accountname)
+#print(bpcanidatelist)
+bpcanidatelist.sort()
+votecmdlist = ['python','main.py','voteproducer']
+for conffile in ["voters.conf"]:
+    with open(conffile,'r') as f:
+        for line in f:
+            accountname = line.split(':')[0]
+            config['voteproducer']['voter'] = accountname
+            config['voteproducer']['account'] = accountname
+            config['voteproducer']['producers'] = ','.join(bpcanidatelist[23:-1])
+            cfgfile = open(configfile,'w')
+            config.write(cfgfile)
+            cfgfile.close()
+            #ownerkey = (line.split(':')[1].strip())
+            subprocess.run(votecmdlist)
