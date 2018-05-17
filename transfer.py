e58973d7bb992fcc75b4383d93d3a756cdfc0a91
sam <samyankun@gmail.com>
2018-05-17 15:08:25 +0800

    update balance get

--- transfer.py
+++ transfer.py
@@ -7,15 +7,19 @@ dirname = os.path.dirname(__file__)
 configfile = os.path.join(dirname, './config.ini')
 config.read(configfile)
 transfer = []
-for conffile in ["voters.conf","accounts.conf"]:
+balanceaccount=["eosio.token"]
+for conffile in ["accounts.conf","voters.conf"]:
     with open(conffile,'r') as f:
         for line in f:
             accountname = line.split(':')[0]
+            balanceaccount.append(accountname)
             tmp = "%s %s %s %s" % ("eosio.token",accountname,"1000.0000","EOS")
             transfer.append(tmp)
 transfer = ';'.join(transfer)
+balanceaccount = ','.join(balanceaccount)
 print(transfer)
 config['transfer']['item'] = transfer
+config['balance']['account'] = balanceaccount
         #ownerkey = (line.split(':')[1].strip())
 with open(configfile,'w') as configfile:
     config.write(configfile)
